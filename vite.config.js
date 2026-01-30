4a7e4d19176d8d12bb32236a3bffd3325282cd57
phxbinh <phxbinh@gmail.com>
2026-01-30 11:44:38 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,4 @@
+/*
 import { defineConfig } from 'vite';
 
 export default defineConfig({
@@ -8,4 +9,25 @@ export default defineConfig({
   server: {
     open: true,
   },
+});
+*/
+// vite.config.js
+import { defineConfig } from 'vite';
+
+export default defineConfig(({ command }) => {
+  const isSSR = command === 'build' && process.argv.includes('--ssr');
+
+  return {
+    build: {
+      outDir: isSSR ? 'dist/server' : 'dist/client',
+      emptyOutDir: true,
+      ssr: isSSR ? 'src/entry-server.js' : false,  // entry server mới
+      rollupOptions: {
+        input: isSSR ? undefined : 'index.html',  // client giữ nguyên
+      },
+    },
+    server: {
+      open: true,
+    },
+  };
 });
